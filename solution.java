210c56a514c674f9b7df4720f65d34f5df0a457d
PLgitdev <50424549+PLgitdev@users.noreply.github.com>
2020-11-12 21:01:58 -0800

    Create solution.java
    Java solution

---
+++ solution.java
@@ -0,0 +1,21 @@
+ static void checkMagazine(String[] magazine, String[] note) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String i : note) {
+            if(map.containsKey(i)) {
+                int value = map.get(i);
+                map.put(i, value += 1);
+            }
+            else map.put(i, 1);
+        }
+        for(String i : magazine) {
+            if(map.containsKey(i)) {
+                int value = map.get(i);
+                if(map.get(i) == 1) {
+                    map.remove(i);
+                }
+                else map.put(i, value -= 1);
+            }
+        }
+        String result = map.isEmpty() ? "Yes" : "No";
+        System.out.println(result);
+    }
